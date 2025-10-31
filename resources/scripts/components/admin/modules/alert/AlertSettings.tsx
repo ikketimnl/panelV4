0a1782bdba605463ae92436c49c43b6607f88c51
ikketim <info@timvds.nl>
2025-10-31 16:24:04 +0100

    Created the option "ikketim"
    Added a custom option for the "ikketim" panel

--- resources/scripts/components/admin/modules/alert/AlertSettings.tsx
+++ resources/scripts/components/admin/modules/alert/AlertSettings.tsx
@@ -88,6 +88,7 @@ export default () => {
                                     <option value={'info'}>Info (Blue)</option>
                                     <option value={'warning'}>Warning (Yellow)</option>
                                     <option value={'danger'}>Danger (Red)</option>
+                                    <option vallue={'ikketim'}>Ikketim (Themed)</option>
                                 </Select>
                             </div>
                             <p className={'text-gray-400 text-xs mt-1.5'}>
