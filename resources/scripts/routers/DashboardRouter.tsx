06c7336df8c925052eb5b9fdb4e498d58b40570c
ikketim <info@timvds.nl>
2025-11-01 19:04:49 +0100

    Add logo image to DashboardRouter component

--- resources/scripts/routers/DashboardRouter.tsx
+++ resources/scripts/routers/DashboardRouter.tsx
@@ -62,7 +62,11 @@ function DashboardRouter() {
                     onClick={() => setCollapsed(!collapsed)}
                 >
                     {!collapsed ? (
-                        <h1 className={'text-2xl text-neutral-50 whitespace-nowrap font-medium'}>{name}</h1>
+                        <img
+                            src={logo?.toString() || 'https://ikketim.nl/wp-content/uploads/2025/09/cropped-ikketim-logo-new.png'}
+                            className={'mt-4 w-12'}
+                            alt={'Logo'}
+                        />
                     ) : (
                         <img
                             src={logo?.toString() || 'https://ikketim.nl/wp-content/uploads/2025/09/cropped-ikketim-logo-new.png'}
