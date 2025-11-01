48f880c5251786b9d5636a654a9d9d0bc078fb74
ikketim <info@timvds.nl>
2025-11-01 20:35:22 +0100

    Update logo source in ServerRouter component

--- resources/scripts/routers/ServerRouter.tsx
+++ resources/scripts/routers/ServerRouter.tsx
@@ -110,7 +110,7 @@ function ServerRouter() {
                     >
                     {!collapsed ? (
                         <img
-                            src={settings.logo?.toString() || 'https://ikketim.nl/wp-content/uploads/2025/10/HD-Banner.jpg'}
+                            src={settings.logo?.toString() || 'https://ikketim.nl/wp-content/uploads/2025/11/BannerLogo3.png'}
                             className={'mt-4 w-12'}
                             alt={'Logo'}
                         />
