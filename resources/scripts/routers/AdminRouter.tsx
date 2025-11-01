2ca23bd478b5ef518086a635d7be6e6b72740e78
ikketim <info@timvds.nl>
2025-11-01 19:12:44 +0100

    Update logo source in AdminRouter component

--- resources/scripts/routers/AdminRouter.tsx
+++ resources/scripts/routers/AdminRouter.tsx
@@ -45,7 +45,7 @@ function AdminRouter() {
                 >
                     {!collapsed ? (
                         <img
-                            src={settings.logo?.toString() || 'https://ikketim.nl/wp-content/uploads/2025/09/cropped-ikketim-logo-new.png'}
+                            src={settings.logo?.toString() || 'https://ikketim.nl/wp-content/uploads/2025/10/HD-Banner.jpg'}
                             className={'mt-4 w-12'}
                             alt={'Logo'}
                         />
